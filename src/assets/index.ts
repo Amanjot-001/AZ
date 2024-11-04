dd22f5a9022a0a92f7f057b938392af7814c0a50
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-11-04 22:18:30 +0530

    header added

---
+++ src/assets/index.ts
@@ -0,0 +1,3 @@
+export { default as MenuLogo } from './menu.png'
+export { default as BellLogo } from './bell.png'
+export { default as DashLogo } from './dashboard.png'
---
+++ src/components/sections/header/header.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BellLogo, MenuLogo } from "../../../assets";
+
+const Header: React.FC = () => {
+return (
+		<header className="flex items-center justify-between p-4 bg-white shadow-md">
+			<div className="flex items-center">
+				<img src={MenuLogo} alt="menu" className="mr-2 w-6 h-6" />
+				<span className="text-xl font-bold text-gray-800">AlgoZenith</span>
+			</div>
+
+			<div className="flex items-center">
+				<img src={BellLogo} alt="notification" className="mr-4 w-6 h-6 cursor-pointer" />
+				<div className="w-9 h-9 rounded-full bg-gray-300 cursor-pointer flex items-center justify-center">
+					<span className="text-gray-600">P</span>
+				</div>
+			</div>
+		</header>
+	);
+}
+
+export default Header;

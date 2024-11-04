e53554050e5cbbd99306ad9ac8146dc4926e425e
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-11-04 23:55:05 +0530

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { Header, Sidebar } from "./components/sections"
+import { Header, Sidebar } from "./components/sections";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
 	return (
-		<>
+		<div className="flex flex-col min-h-screen bg-primary-gradient">
 			<Header />
-			<Sidebar />
-		</>
-	)
+
+			<div className="flex">
+				<Sidebar />
+				<main className="p-4 w-full bg-secondary_bg">
+					<Routes>
+						<Route path="/" element={<Home />} />
+					</Routes>
+				</main>
+			</div>
+		</div>
+	);
 }
 
-export default App
+const Home = () => <div>Home Page</div>;
+
+export default App;

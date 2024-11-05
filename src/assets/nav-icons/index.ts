09566bc62d11ac2c40a96ba1d78a99059e99e700
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-11-06 01:33:25 +0530

    nav icons fixed

--- src/assets/nav-icons/index.ts
+++ src/assets/nav-icons/index.ts
@@ -1,3 +1,3 @@
-export { default as CalendarIcon } from "./calendar.png"
-export { default as BriefcaseIcon } from "./briefcase.png"
-export { default as InfoIcon } from "./info.png"
+export { default as CalendarIcon } from "./calendar.svg"
+export { default as BriefcaseIcon } from "./briefcase.svg"
+export { default as InfoIcon } from "./info.svg"

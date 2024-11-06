048d6d924a64415765cf86e0ab2efb6e38ffe492
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-11-06 12:16:04 +0530

    fixed naming

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,3 +1,3 @@
-export { default as Header } from "./header/header.tsx"
-export { default as Sidebar } from "./sidebar/sidebar.tsx"
+export { default as Header } from "./header/Header.tsx"
+export { default as Sidebar } from "./sidebar/Sidebar.tsx"
 export { default as MainContent } from "./main/MainContent.tsx"
